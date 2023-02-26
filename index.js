11b48709f397e6d6201f2c824ab4fa96f1112556
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-02-26 14:03:26 -0600

    css+js

---
+++ index.js
@@ -0,0 +1,10 @@
+let price1 = document.getElementById('price1');
+let a = price1.innerHTML;
+let price2 = document.getElementById('price2');
+let b = price2.innerHTML;
+let price3 = document.getElementById('price3');
+let c = price3.innerHTML;
+let price4 = document.getElementById('price4');
+let d = price4.innerHTML;
+let sum = document.getElementById('sum');
+sum.textContent = Number(a)+Number(b)+Number(c)+Number(d);
